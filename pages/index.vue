8c0dc0b7256cecb4c813bdb753fec5d4411aaa6d
ponytojas <daniel.villalobosdel@gmail.com>
2021-04-17 21:06:25 +0200

    ✨  Completly new index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">ponytojas</h1>
-      <div class="links">
-        <NuxtLink to="/ColorSchema">Color Schema</NuxtLink>
+    <div class="mt-20 md:mt-0">
+      <div class="px-2 md:px-0">
+        <span class="text-gray-600 font-thin title"
+          >Hi there<span style="color: #FF6D70">!</span> I'm <span class="font-normal">Daniel Villalobos</span></span
+        >
+      </div>
+      <div class="p-4 md:p-6">
+        <p class="text-gray-600 font-light subtitle">
+          I'm a
+          <span class="line-through title-gradient-text">(bad)</span>
+          developer, UI/UX passionate and technology lover
+        </p>
       </div>
     </div>
   </div>
@@ -24,21 +31,39 @@ export default {}
   text-align: center;
 }
 
+.title-gradient-text {
+  text-decoration: line-through;
+  background: linear-gradient(
+    to right,
+    #4fc3f7,
+    #6ed477,
+    #6ED477
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
 .title {
-  font-family: 'Inter';
   display: block;
-  font-weight: 300;
   font-size: 100px;
   color: #35495e;
-  letter-spacing: 1px;
 }
 
 .subtitle {
-  font-weight: 300;
   font-size: 42px;
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
+  letter-spacing: 1px;
+}
+
+@media (max-width: 768px) {
+  .title {
+    font-size: 3.5rem;
+  }
+  .subtitle {
+    font-size: 2rem;
+  }
 }
 
 .links {
