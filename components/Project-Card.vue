12f8420eaef297ed5be43fe6099a77b21fc02f54
ponytojas <daniel.villalobosdel@gmail.com>
2021-04-17 22:55:00 +0200

    🚧  WIP: Project page

---
+++ components/Project-Card.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="border rounded bg-white card shadow flex flex-col"
+    style="width: 350px; height: 350px"
+  >
+    <img :src="require(`~/assets/${image}`)" />
+    <p class="mt-4 text-4xl text-gray-900">{{ name }}</p>
+
+    <button
+      class="w-6/12 mt-8 rounded mx-auto text-white px-4 py-2"
+      :style="{ 'background-color': lightColor  }"
+    >
+      <nuxt-link :to="link">Check more</nuxt-link>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    link: String,
+    image: String,
+    lightColor: String,
+    darkColor: String
+  },  
+}
+</script>
+
+<style>
+.card {
+  transition: all 0.5s cubic-bezier(0.25, 0.8, 0.25, 1);
+}
+.card:hover {
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+}
+</style>
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,11 +1,40 @@
 <template>
-    <div>
-        <p>Projects</p>
+  <div class="container flex flex-row flex-wrap mt-4">
+    <div class="m-4" v-for="project in this.projects" :key="project.id">
+      <Project-Card
+        :name="project.name"
+        :link="project.link"
+        :image="project.image"
+        :lightColor="project.lightColor"
+        :darkColor="project.darkColor"
+      />
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    transition: 'fade'
+  transition: 'fade',
+  data() {
+    return {
+      projects: [
+        {
+          id: 1,
+          name: 'fagolambda',
+          link: '/fagolambda',
+          image: 'fagolambda.png',
+          color: 'green',
+        },
+        {
+          id: 1,
+          name: 'enClase',
+          link: '/enclase',
+          image: 'enclase.png',
+          lightColor: '#4fc3f7',
+          darkColor: '#2196F3',
+        },
+      ],
+    }
+  },
 }
 </script>
