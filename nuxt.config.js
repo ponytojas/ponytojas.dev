bc277b3715f22f17816ee6b59b0a3b9641fb23e1
ponytojas <daniel.villalobosdel@gmail.com>
2021-04-25 17:22:35 +0200

    🔍  Improve lighthouse

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,14 @@
 export default {
   target: 'static',
   head: {
+    htmlAttrs: {
+      lang: 'en'
+    },
     title: 'Daniel Villalobos',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: "Daniel Villalobos's personal website" },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
