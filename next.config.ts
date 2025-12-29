5819b93c7a6a2cd1f960928b740bff5c5df87bc7
Daniel Villlalobos <daniel.villalobosdel@icloud.com>
2025-12-29 18:20:42 +0100

    Remove standalone output

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,12 @@
 import type { NextConfig } from "next";
-import createMDX from '@next/mdx'
+import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-    output: "standalone",
-
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
 const withMDX = createMDX({
   extension: /\.(md|mdx)$/,
-})
+});
 
-export default withMDX(nextConfig)
+export default withMDX(nextConfig);
