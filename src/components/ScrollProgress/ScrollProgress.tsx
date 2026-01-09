28424daeedc0aa2e4b83ff3a6234c60daf9a5e53
Daniel Villlalobos <daniel.villalobosdel@icloud.com>
2026-01-09 16:09:07 +0100

    Use brand color

--- src/components/ScrollProgress/ScrollProgress.tsx
+++ src/components/ScrollProgress/ScrollProgress.tsx
@@ -1,19 +1,19 @@
-'use client';
+"use client";
 
-import { motion, useScroll, useSpring } from 'motion/react';
+import { motion, useScroll, useSpring } from "motion/react";
 
 export const ScrollProgress = () => {
-    const { scrollYProgress } = useScroll();
-    const scaleX = useSpring(scrollYProgress, {
-        stiffness: 100,
-        damping: 30,
-        restDelta: 0.001
-    });
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
 
-    return (
-        <motion.div
-            className="fixed top-0 left-0 right-0 h-0.5 bg-foreground/20 origin-left z-[100]"
-            style={{ scaleX }}
-        />
-    );
+  return (
+    <motion.div
+      className="fixed top-0 left-0 right-0 h-0.5 bg-brand-primary/80 origin-left z-100"
+      style={{ scaleX }}
+    />
+  );
 };
